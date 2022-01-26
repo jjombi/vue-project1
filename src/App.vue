3198cc65e584e19ee05ff65925a958039e198be4
sis01066745950 <67420558+sis01066745950@users.noreply.github.com>
2022-01-26 21:34:19 +0900

    vue project-2

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,9 @@
 <template>
 
-    <div>
-        <h1>{{title}}</h1>
-        <input v-model="title">
-    </div>
-    <div>
-        <input type="checkbox" id="선택" value="선택" v-model="checkedWebtoons">
-        <label for="선택">선택</label>
-        <input type="checkbox" id="선택-2" value="선택-2" v-model="checkedWebtoons">
-        <label for="선택-2">선택-2</label>
-        <br>
-        <span>선택 보기 : {{ checkedWebtoons }} </span>
-    </div>
+<div>
+    <router-view/>
+</div>
+    
 
 </template>
 
@@ -19,13 +11,13 @@
     export default {
         data() {
             return {
-                title: "",
-                checkedWebtoons:[],
-            };
+                firstname: '',
+                lastname: '',
+            }
         },
-
-
-    };
+     
+    }
+   
 </script>
 <style scoped>
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,19 +1,15 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import About from "./views/About.Vue";
 
-const routes = [
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
-]
 
-const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes
-})
+const router = new VueRouter({
+  mode: "history",
+  routes: [
+    { path: "/", component: About },
+    {
+
+    }
+  ]
+});
+
 
 export default router
