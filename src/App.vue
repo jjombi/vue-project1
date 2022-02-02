dd7648e2055f1bdd84bd5a8853d3224f528c8d9d
sis01066745950 <67420558+sis01066745950@users.noreply.github.com>
2022-02-02 21:13:15 +0900

    Revert "vue project-2"
    This reverts commit 3198cc65e584e19ee05ff65925a958039e198be4.

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,17 @@
 <template>
 
-<div>
-    <router-view/>
-</div>
-    
+    <div>
+        <h1>{{title}}</h1>
+        <input v-model="title">
+    </div>
+    <div>
+        <input type="checkbox" id="선택" value="선택" v-model="checkedWebtoons">
+        <label for="선택">선택</label>
+        <input type="checkbox" id="선택-2" value="선택-2" v-model="checkedWebtoons">
+        <label for="선택-2">선택-2</label>
+        <br>
+        <span>선택 보기 : {{ checkedWebtoons }} </span>
+    </div>
 
 </template>
 
@@ -11,13 +19,13 @@
     export default {
         data() {
             return {
-                firstname: '',
-                lastname: '',
-            }
+                title: "",
+                checkedWebtoons:[],
+            };
         },
-     
-    }
-   
+
+
+    };
 </script>
 <style scoped>
 </style>
--- src/main.js
+++ src/main.js
@@ -4,10 +4,3 @@ import router from './router'
 import store from './store'
 
 createApp(App).use(store).use(router).mount('#app')
-
-Vue.config.productionTip = false;
-
-new Vue({
-  router,
-  render: h => h(App)
-}).$mount("#app");
